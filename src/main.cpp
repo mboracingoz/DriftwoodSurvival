bf6562169a5f9b0091e036e817525451ba7d49e0
mboracingoz <mboracingoz@gmail.com>
2026-04-29 13:55:29 +0300

    Added basic player movement with deltaTime and WASD input

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,42 @@ int main()
     InitWindow(1280, 720, "Driftwood: Survival Prototype");
     SetTargetFPS(60);
 
+    Vector2 playerPos = {640.0f, 360.0f};
+    float playerSpeed = 250.0f;
+
     while (!WindowShouldClose())
     {
+        float deltaTime = GetFrameTime();
+
+        if (IsKeyDown(KEY_W))
+        {
+            playerPos.y -= playerSpeed * deltaTime;
+        }
+
+        if (IsKeyDown(KEY_S))
+        {
+            playerPos.y += playerSpeed * deltaTime;
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            playerPos.x -= playerSpeed * deltaTime;
+        }
+
+        if (IsKeyDown(KEY_D))
+        {
+            playerPos.x += playerSpeed * deltaTime;
+        }
+        
+
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         DrawText("Driftwood: Survival Prototype", 40, 40, 30, DARKGREEN);
-        DrawText("Raylib + C++ setup is working.", 40, 90, 20, DARKGRAY);
+        DrawText("Move with WASD keys", 40, 80, 20, DARKGRAY);
+
+        DrawRectangleV(playerPos, {40.0f, 40.0f}, RED);
 
         EndDrawing();
     }
