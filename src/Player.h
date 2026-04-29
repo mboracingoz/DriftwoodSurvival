1ec15bdf9cf2599d33bc28da9e21176803e1e507
mboracingoz <mboracingoz@gmail.com>
2026-04-29 13:59:08 +0300

    Extract player data into Player struct

---
+++ src/Player.h
@@ -0,0 +1,9 @@
+#pragma once 
+#include "raylib.h"
+
+struct Player
+{
+    /* data */
+    Vector2 position;
+    float speed;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include "raylib.h"
+#include "Player.h"
 
 int main()
 {
     InitWindow(1280, 720, "Driftwood: Survival Prototype");
     SetTargetFPS(60);
 
-    Vector2 playerPos = {640.0f, 360.0f};
-    float playerSpeed = 250.0f;
+    Player player{
+        {640.0f, 360.0f},
+        250.0f
+    };
 
     while (!WindowShouldClose())
     {
@@ -14,22 +17,22 @@ int main()
 
         if (IsKeyDown(KEY_W))
         {
-            playerPos.y -= playerSpeed * deltaTime;
+            player.position.y -= player.speed * deltaTime;
         }
 
         if (IsKeyDown(KEY_S))
         {
-            playerPos.y += playerSpeed * deltaTime;
+            player.position.y += player.speed * deltaTime;
         }
 
         if (IsKeyDown(KEY_A))
         {
-            playerPos.x -= playerSpeed * deltaTime;
+            player.position.x -= player.speed * deltaTime;
         }
 
         if (IsKeyDown(KEY_D))
         {
-            playerPos.x += playerSpeed * deltaTime;
+            player.position.x += player.speed * deltaTime;
         }
         
 
@@ -40,7 +43,7 @@ int main()
         DrawText("Driftwood: Survival Prototype", 40, 40, 30, DARKGREEN);
         DrawText("Move with WASD keys", 40, 80, 20, DARKGRAY);
 
-        DrawRectangleV(playerPos, {40.0f, 40.0f}, RED);
+        DrawRectangleV(player.position, { 40.0f, 40.0f }, BROWN);
 
         EndDrawing();
     }
